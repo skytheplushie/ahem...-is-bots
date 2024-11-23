a3d08a4dd70251fa0c59e619c54f35e01d4e532b
skytheplushie <ole16307@gmail.com>
2024-11-23 19:27:42 +0500

    Add files via upload

--- botty bot5.py
+++ botty bot5.py
@@ -5,7 +5,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = '7680989311:AAEIRSxJCLJkGTza1H1Kci3e_m2LVRYwo3A'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
